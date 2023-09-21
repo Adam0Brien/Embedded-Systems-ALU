c379845fe613f22537cd48439e4eefe6311af408
Adam O'Brien <20093460@mail.wit.ie>
2023-09-22 00:16:42 +0100

    Add operations and update library in alu.vhd

--- cpu1_ALU_VHDL_Source/alu.vhd
+++ cpu1_ALU_VHDL_Source/alu.vhd
@@ -18,11 +18,11 @@
 --
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -47,7 +47,24 @@ case sel is
 	out_alu<= inp_a; --alupassOp
 	when "0001" => 
 	out_alu<= inp_a and inp_b; --andOp
-	
+	when "0010" =>
+	out_alu<= inp_a or inp_b; -- orOp
+	when "0011" =>
+	out_alu<= not inp_a; -- notOp
+	when "0100" =>
+	out_al<= inp_a xor inp_b; -- xorOp
+	when "0101" =>
+	out_alu<= inp_a + inp_b; --plusOp
+	when "0110" =>
+	out_alu<= inp_a - inp_b; -- minusOp
+	when "0111" =>
+	out_alu<= inp_a + 1; -- incOp
+	when "1000" =>
+	out_alu<= inp_a - 1; -- decOp
+	when "1001" =>
+	out_alu<= 0; -- clearOp
+	when others =>
+	NULL;
 end case;
 
 end process;
